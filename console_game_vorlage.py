093b242823e13d0b66fa7e2c7e4f35b2a40a5b32
Majakuehn <kuhn.maja.2021@ksz.edu-zg.ch>
2025-01-20 14:36:47 +0100

    hausaufgaben Maja

--- console_game_vorlage.py
+++ console_game_vorlage.py
@@ -6,3 +6,10 @@ Spalte = 7
 def create_board():
     return [[" " for _ in range(Zeile)] for _ in range(Spalte)]
 
+
+def display_board(board):
+    print("\n  " + "   ".join(str(i+1) for i in range(Zeile)))  # Spaltennummern
+    print(" +" + "---+" * Zeile)  # obere Begrenzung
+    for row in board:
+        print(" | " + " | ".join(Spalte) + " |")  # Zeilen mit Inhalten
+        print(" +" + "---+" * Zeile)  # untere Begrenzung
