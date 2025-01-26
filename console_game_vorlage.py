2b9b18c15b0abd3c44a283a438d948d5f0e844ad
Majakuehn <kuhn.maja.2021@ksz.edu-zg.ch>
2025-01-26 20:02:20 +0100

    Update console_game_vorlage.py
    H: Spiel überarbeitet, aber irgendwie geht es jetzt nicht mehr, es kommt sie Frage welche Spalte man wählen will nicht. Dilara und ich haben zusammen an meinem Computer gearbeitet, weil sie bei mir war und sie ihr Ipad nicht dabei hatte.

--- console_game_vorlage.py
+++ console_game_vorlage.py
@@ -1,29 +1,60 @@
-Zeile = 7
-Spalte = 7
-
+import random
 board = [
-    [' ' for _ in range(Spalte)] for _ in range(Zeile)
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 ]
+PLAYERS = {1: "O", 2: "X"}
+#print(PLAYERS[1])
+def print_board():
+    erste_zeile =   "|" + board[0][1] + "|" + board[0][2] + "|" + board[0][3] + "|" + board[0][4] + "|" + board[0][5] + "|" + board[0][6] + "|" + board[0][7] + "|"  + "\n"
+    zwischenzeile = "-----------------" + "\n"
+    zweite_zeile =  "|" + board[1][1] + "|" + board[1][2] + "|" + board[1][3] + "|" + board[1][4] + "|" + board[1][5] + "|" + board[1][6] + "|" + board[1][7] + "|"  + "\n"
+    dritte_zeile =  "|" + board[2][1] + "|" + board[2][2] + "|" + board[2][3] + "|" + board[2][4] + "|" + board[2][5] + "|" + board[2][6] + "|" + board[2][7] + "|"  +"\n"
+    vierte_zeile =  "|" + board[3][1] + "|" + board[3][2] + "|" + board[3][3] + "|" + board[3][4] + "|" + board[3][5] + "|" + board[3][6] + "|" + board[3][7] + "|"  + "\n"
+    fünfte_zeile =  "|" + board[4][1] + "|" + board[4][2] + "|" + board[4][3] + "|" + board[4][4] + "|" + board[4][5] + "|" + board[4][6] + "|" + board[4][7] + "|" + "\n"
+    sechste_zeile =  "|" + board[5][1] + "|" + board[5][2] + "|" + board[5][3] + "|" + board[5][4] + "|" + board[5][5] + "|" + board[5][6] + "|" + board[5][7] + "|"  + "\n"
+    print(erste_zeile  +  zweite_zeile  + dritte_zeile  + vierte_zeile  + fünfte_zeile  + sechste_zeile + zwischenzeile)
+print_board()
 
 PLAYERS = {1: "O", 2: "X"}
 
+
+
 def print_board():
-    spielfeld = ""
-    for zeile in board:
-        spielfeld += "|".join(zeile) + "\n"
-        spielfeld += "-" * (2 * len(zeile) - 1) + "\n"
-    print(spielfeld)
+    for row in board:
+        print("|" + "|".join(row) + "|")
+    print(" " + " ".join(map(str, range(1, 8))))
+
+
 
 def play_one_round(board, player):
     while True:
-        user_input = input(f"Player {player} ({PLAYERS[player]}), wähle eine Spalte (1-{Spalte}): ")
-        
-        if user_input== 
-        spalte = int(user_input) - 1
-        if spalte < 0 or spalte >= Spalte:
-            print(f"Ungültige Spalte. Bitte wähle eine Zahl zwischen 1 und {Spalte}.")
-            continue
-           
+        spalte = int(input("Gib eine Spalte ein (1-7): "))
+        if spalte >= 1 and spalte <= 7:
+            spalte -= 1
+            
+            for zeile in range(5, -1, -1):
+                if board[zeile][spalte] == " ":
+                    board[zeile][spalte] = PLAYERS[player]
+                    return
+            print("Spalte voll. Wähle eine andere.")
+        else:
+            print("Input inkorrekt. Wähle eine Spalte zwischen 1 und 7.")
+
+def check_winner(board, player):
+    symbol = PLAYERS[player]
+
+    for zeile in range(6):
+        for spalte in range(4):
+            if (board[zeile][spalte] == symbol and
+                board[zeile][spalte + 1] == symbol and
+                board[zeile][spalte + 2] == symbol and
+                board[zeile][spalte + 3] == symbol):
+                return player
 
 
 
